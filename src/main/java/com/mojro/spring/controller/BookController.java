e765886b215a31b560b1a35e2379bba7329adb3d
Venkatesh Gudi <vggudi@gmail.com>
2020-03-13 18:44:11 +0530

    GET PUT POST DELETE all are working

---
+++ src/main/java/com/mojro/spring/controller/BookController.java
@@ -0,0 +1,74 @@
+package com.mojro.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import com.mojro.spring.dao.BookDAO;
+import com.mojro.spring.model.Book;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+	@Autowired
+	private BookDAO bookDAO;
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String welcome() {
+		return "Welcome to RestTemplate Example.";
+	}
+
+	@RequestMapping(value = "/books", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+	@ResponseBody
+	public List<Book> getBooks() {
+		List<Book> list = bookDAO.getAllBooks();
+		return list;
+	}
+
+	@RequestMapping(value = "/book/{ISBN}", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+	@ResponseBody
+	public Book getBook(@PathVariable("ISBN") String ISBN) {
+		return bookDAO.getBook(ISBN);
+	}
+
+	@RequestMapping(value = "/book", method = RequestMethod.POST, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public @ResponseBody Book addBook(@RequestBody Book book) {
+
+		return bookDAO.addBook(book);
+
+	}
+
+	/*
+	 * @PostMapping(value = "/book") public ResponseEntity<?>
+	 * addBook(@RequestBody Book book) {
+	 * 
+	 * bookDAO.addBook(book);
+	 * 
+	 * return new ResponseEntity<Book>(book, HttpStatus.OK); }
+	 */
+
+	@RequestMapping(value = "/book", method = RequestMethod.PUT, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public 	@ResponseBody Book updateBook(@RequestBody Book book) {
+
+		return bookDAO.updateBook(book);
+	}
+
+	@RequestMapping(value = "/books/{ISBN}", //
+			method = RequestMethod.DELETE, //
+			produces = { MediaType.APPLICATION_JSON_VALUE })
+	@ResponseBody
+	public void deleteBook(@PathVariable("ISBN") String ISBN) {
+		bookDAO.deleteBook(ISBN);
+	}
+}
---
+++ src/main/java/com/mojro/spring/model/Book.java
@@ -0,0 +1,52 @@
+package com.mojro.spring.model;
+
+public class Book {
+	private String ISBN;
+	private String name;
+	private String author;
+	private double price;
+
+	public Book() {
+
+	}
+
+	public Book(String ISBN, String name, String author, double price) {
+		this.ISBN = ISBN;
+		this.name = name;
+		this.author = author;
+		this.price = price;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String ISBN) {
+		this.ISBN = ISBN;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
